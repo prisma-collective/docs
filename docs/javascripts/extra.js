70716f2a12e8630d71b6a891156504fe9da5cdbe
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2024-10-22 03:20:47 -0400

    Font changes. Tried to link logo to prisma site.

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var logoLink = document.querySelector('.md-nav__button.md-logo');
+    if (logoLink) {
+        logoLink.href = 'https://prisma.events';
+    }
+});
