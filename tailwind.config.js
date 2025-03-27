ef346d460cbd6f4f0f6bb055bdc1fe7ccaa5e6c7
tbsfchnr <tobias.fechner@hotmail.co.uk>
2025-03-28 00:11:58 +0100

    vault backup: 2025-03-28 00:11:58

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: "class", // Enables dark mode support
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
