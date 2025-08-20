00369abd88ebb4064ec9873dc81a782bebea0dc3
tbsfchnr <tobias.fechner@hotmail.co.uk>
2025-08-20 19:27:34 +0200

    add redirects for broken links on arb comments

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,19 @@ const withNextra = nextra({
 })
 
 export default withNextra({
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/processes/process-infrastructuring/timeli',
+        destination: '/processes/process-infrastructuring/timelining',
+        permanent: false, // Set to false for temporary redirect (302)
+      },
+      {
+        source: '/processes/ground-potentialising/deplo',
+        destination: '/processes/ground-potentialising/deploy',
+        permanent: false, // Set to false for temporary redirect (302)
+      }
+    ]
+  },
 })
