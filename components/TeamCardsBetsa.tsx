749c63099b80000f9c02a4ae256452c18c728492
Betsa777 <dakuyobetsaleel48@gmail.com>
2025-12-12 13:50:02 +0000

    adding 0xTeam members cards

--- components/TeamCardsBetsa.tsx
+++ components/TeamCardsBetsa.tsx
@@ -35,7 +35,7 @@ interface TeamCardsProps {
   team?: TeamMember[];
 }
 
-export default function TeamCardsOx({ team }: TeamCardsProps) {
+export default function TeamCardsBetsa({ team }: TeamCardsProps) {
   const dataToUse = team ?? (teamData as TeamMember[]);
 
   // Global cache for Telegram data
